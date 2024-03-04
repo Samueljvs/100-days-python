56a9066c5b2ce155a19a2b11e67750c2521e56a3
Samuel Verevis <sam.john.vs@gmail.com>
2024-03-04 13:47:34 +0330

    Starting day-25

--- Day-24/Mail Merge Project Start/main.py
+++ Day-24/Mail Merge Project Start/main.py
@@ -13,8 +13,12 @@ for x in names:
     new_letter = letter.replace("[name]", x)
     with open("Day-24/Mail Merge Project Start/Output/ReadyToSend/" + x + ".txt", mode = "w") as file:
         file.write(new_letter)
+
+
     
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+        
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
